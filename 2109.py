f0be67925d45665ba77a51aca25f7739a2a7d3c0
Javohir70707 <zavohirmamurzonov511@gmail.com>
2023-09-22 18:01:01 +0500

    message

--- 2109.py
+++ 2109.py
@@ -11,16 +11,3 @@
 # 5007669544563319,mastercard,Euro,EUR,Major Chemicals,France
 # 5108756348035400,mastercard,Euro,EUR,Farming/Seeds/Milling,France
 
-def count_countries_with_mastercard(filename):
-    countries = set()
-    
-    with open(filename, 'r') as file:
-        for line in file:
-            data = line.strip().split(',')
-            if 'mastercard' in data[1].lower():
-                countries.add(data[5])
-    return len(countries)
-
-filename = "Karta.txt"
-count = count_countries_with_mastercard(filename)
-print(count)
